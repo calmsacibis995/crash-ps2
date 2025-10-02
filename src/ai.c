3da9ed0749f3d3af40a9f73a6f2a720365c5e72b
Stefanos Stefanidis <www.fe32gr23@gmail.com>
2025-10-02 10:35:13 +0300

    Proceed with decomp.

---
+++ src/ai.c
@@ -0,0 +1,4 @@
+void
+ResetAI(void)
+{
+}
---
+++ src/edwumpa.c
@@ -0,0 +1,17 @@
+/*
+ * @unimplemented
+ */
+void
+edwmpDELETE(void)
+{
+}
+
+/*
+ * @implemented
+ * @unused
+ */
+void
+edwmpClose(void)
+{
+	eduiMenuDestroy(edwmp_options_menu);
+}
