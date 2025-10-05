8f64fda50ec85fcee6b1570b690d995fa0c8ba23
Stefanos Stefanidis <www.fe32gr23@gmail.com>
2025-10-05 13:09:52 +0300

    Finish NuStrStr().

--- nu2.ps2/nucore/nustring.c
+++ nu2.ps2/nucore/nustring.c
@@ -66,4 +66,42 @@ NuStrLen(const char *str)
 	return len;
 }
 
+char *
+NuStrStr(const char *haystack, const char *needle)
+{
+	char c;
+	char *needle_tmp, *haystack_tmp;
 
+	c = *haystack;
+	do {
+		if (c == '\0')
+			return NULL;
+		if ((long)*needle == 0)
+			return haystack;
+		if ((long)(int)*haystack == 0)
+			c = *needle;
+		else if ((long)(int)*haystack == (long)*needle) {
+			needle_tmp = needle + 1;
+			haystack_tmp = haystack;
+			while (TRUE) {
+				haystack_tmp++;
+				if (*needle_tmp == '\0')
+					return haystack;
+				if (*haystack_tmp == '\0') {
+					c = *needle_tmp;
+					goto loop;
+				}
+				if (*haystack_tmp != *needle_tmp)
+					break;
+				needle_tmp++;
+			}
+			c = *needle_tmp;
+		} else
+			c = *needle;
+loop:
+		if (c == '\0')
+			return haystack;
+		haystack++;
+		c = *haystack;
+	} while (TRUE);
+}
